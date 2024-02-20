28c8121208b70e00b037484032cb451a5a7a71f7
roylohhh <101264393+roylohhh@users.noreply.github.com>
2024-02-20 15:43:56 +1100

    crud implemented

---
+++ Controllers/InstructorController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using SchoolWebApi.Models;
+using SchoolWebApi.Models.DataManager;
+
+namespace school.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InstructorController : ControllerBase
+{
+    private readonly InstructorManager _repo;
+
+    public InstructorController(InstructorManager repo)
+    {
+        _repo = repo;
+    }
+
+    //GET: api/instructors
+    [HttpGet]
+    public IEnumerable<Instructor> GetInstructors()
+    {
+        return _repo.GetAllInstructors();
+    }
+
+    //GET: api/instructors/1
+    [HttpGet("{InstructorID}")]
+    public Instructor GetInstructors(string id)
+    {
+        return _repo.GetInstructor(id);
+    }
+
+    //POST: api/instructos
+    [HttpPost]
+    public void PostInstructor([FromBody] Instructor instructor)
+    {
+        _repo.AddInstructor(instructor);
+    }
+
+    //DELETE: api/instructors
+    [HttpDelete]
+    public string DeleteInstructor(string InstructorID)
+    {
+        return _repo.DeleteInstructor(InstructorID);
+    }
+
+    //PUT: api/instructors
+    [HttpPut]
+    public void PutInstructor([FromBody] Instructor instructor)
+    {
+        _repo.UpdateInstructor(instructor.InstructorID, instructor);
+    }
+
+}
